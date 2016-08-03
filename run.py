5ca0acb76530a0e04502c033e73d883f8cd933a5
manthan787 <manthant15@gmail.com>
2016-08-03 18:17:03 -0400

    Comments

--- run.py
+++ run.py
@@ -5,11 +5,15 @@ from config import *
 from Indexer import index
 import multiprocessing
 
-
+# Request all the cores as Processors
 MAX_PROCESSES = multiprocessing.cpu_count()
 
 
 def worker(files):
+    """
+    :param files: a list of filenames that are to be used by the worker process
+    :return: Void
+    """
     # Parse XML docs + add features
     parsed_docs = parse_all(files)
 
@@ -18,6 +22,11 @@ def worker(files):
 
 
 def chunk(data):
+    """
+    :param data: All the files containing the blog entries in the data folder
+    :return: List of List containing files chunked based on the number of
+             processes available
+    """
     chunks = []
     chunk_size = int(len(data) / MAX_PROCESSES)
 
@@ -26,14 +35,16 @@ def chunk(data):
     return chunks
 
 
-all_files = os.listdir(DATASET_PATH)
-chunks = chunk(all_files)
-jobs = []
+if __name__ == '__main__':
+    all_files = os.listdir(DATASET_PATH)
+    chunks = chunk(all_files)
+    jobs = []
 
-for i in range(0, MAX_PROCESSES):
-    p = multiprocessing.Process(target=worker, args=([chunks[i]]))
-    jobs.append(p)
-    p.start()
+    # Spawn processes to start processing the data
+    for i in range(0, MAX_PROCESSES):
+        p = multiprocessing.Process(target=worker, args=([chunks[i]]))
+        jobs.append(p)
+        p.start()
 
-# Wait for all the processes to finish
-[job.join() for job in jobs]
+    # Wait for all the processes to finish
+    [job.join() for job in jobs]
